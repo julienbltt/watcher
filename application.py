578aa5818345dc30ea8955f9e129bd93fc92b380
julien <julien.blt@outlook.com>
2024-05-04 20:50:57 +0200

    Adding the logging

--- application.py
+++ application.py
@@ -1,29 +1,48 @@
-""" 
+"""
 Name: application.py
 Author : Julien BALDERIOTTI
 Creation Date: 2024-04-26
-Update Date: 2024-04-26
+Update Date: 2024-05-04
 Description : Application file content main instruction of this application.
+
+/!\ IMPORTANTE NOTE /!\
+> For run the application, you must have 'sudo' group right.
+> If is not the case, the programme may work randomly and some of its main functions may have problems.
+/!\ IMPORTANTE NOTE /!\
 """
 
-# IMPORT Standard libraries #
+# IMPORT libraries #
 from datetime import timedelta
 from time import time
 from sys import argv
 from platform import uname
-
-
-
-# IMPORT main librairies #
+from os import system
 import psutil as psu
+import logging
+import logging.config
+from pathlib import Path
+from json import load, dumps
 
 
 
 # CONSTANTES #
 APP_NAME = argv[0]
+APP_ROOT_PATH = Path(__file__).absolute().parent
+CONFIG_DIR_PATH = APP_ROOT_PATH / "configs"
+LOGGING_CONFIG_FILE_PATH = CONFIG_DIR_PATH / "logging_config.json"
 
 
 
+# LOGGING Initialization #
+logger = logging.getLogger("logger-app")
+def setup_logger(config_file_path):
+    with open(config_file_path) as config_file:
+        config_logging = load(config_file)
+    logging.config.dictConfig(config_logging)
+
+
+
+# Get System Infos #
 def system_info():
     info = {
         "name":uname().node.upper(),
@@ -39,8 +58,23 @@ def system_info():
     }
     if psu.LINUX:
         info["cpu_temp"] = psu.sensors_temperatures()
+
+    logger.debug("System information obtained.\n{}".format(info))
+
     return info
 
+
+
 def app():
-    sys_info = system_info()
-    print("\n".join([f"{key}:{value}" for key, value in sys_info.items()]))
+    # Setup logger #
+    setup_logger(LOGGING_CONFIG_FILE_PATH)
+
+    # Get system informations #
+    sys_infos = system_info()
+
+    # Monitor memory usage #
+    if ( sys_infos['mem_use'] >= 99 ):
+       logger.warning("Almost no usable memory")
+       logger.critical("The system will reboot")
+       system('sudo reboot')
+
